ea78ab29fe748cdc39e1ea3ce985f170d35c109c
Usama-dev1 <raja.usamah@gmail.com>
2026-04-26 21:57:12 +0500

    fixed spacing

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleChat() {
-  const icon = document.querySelector('.widget-body');
-  const iconX = document.querySelector('.icon-x');
-  const iconMsg = document.querySelector('.icon-msg');
-  if (icon.style.display === 'flex') {
-    icon.style.display = 'none';
-    iconX.style.display="none"
-    iconMsg.style.display="flex"
+  const icon = document.querySelector(".widget-body");
+  const iconX = document.querySelector(".icon-x");
+  const iconMsg = document.querySelector(".icon-msg");
+  if (icon.style.display === "flex") {
+    icon.style.display = "none";
+    iconX.style.display = "none";
+    iconMsg.style.display = "flex";
   } else {
-    icon.style.display = 'flex';
-    iconX.style.display="flex"
-    iconMsg.style.display="none"
+    icon.style.display = "flex";
+    iconX.style.display = "flex";
+    iconMsg.style.display = "none";
   }
 }
 
-const chatIcon=document.querySelector(".chat-widget")
-chatIcon.addEventListener("click",toggleChat)
+const chatIcon = document.querySelector(".chat-widget");
+chatIcon.addEventListener("click", toggleChat);
